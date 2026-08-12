5a3b767aaa1750f2da316901f68f7a21e0f374e6
juuso.vartiainen <jv0832@edu.turku.fi>
2026-08-12 12:10:23 +0300

    osa 4

--- analyysi.py
+++ analyysi.py
@@ -17,3 +17,21 @@ print(f"Tekstissä on {len(teksti)} merkkiä.")
 sanat = teksti.split()
 print(f"Tekstissä on {len(sanat)} sanaa.")
 
+# 4 osa
+sanamaarat = {}
+
+for sana in sanat:
+    if sana in sanamaarat:
+        sanamaarat[sana] += 1
+    else:
+        sanamaarat[sana] = 1
+
+jarjestetyt_sanat = sorted(sanamaarat, key=sanamaarat.get, reverse=True)
+
+print(
+    f"Yleisimmät sanat ovat {jarjestetyt_sanat[0]}, "
+    f"{jarjestetyt_sanat[1]}, "
+    f"{jarjestetyt_sanat[2]}, "
+    f"{jarjestetyt_sanat[3]} ja "
+    f"{jarjestetyt_sanat[4]}."
+)
